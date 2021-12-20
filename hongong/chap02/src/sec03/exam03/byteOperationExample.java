22098651b968cbdedcfe70095032ac87a2e39147
Min Jae Kim <exploit017@gmail.com>
2021-12-20 18:19:09 +0900

    java sec3 done

---
+++ hongong/chap02/src/sec03/exam03/byteOperationExample.java
@@ -0,0 +1,17 @@
+package sec03.exam03;
+
+public class byteOperationExample {
+
+	public static void main(String[] args) {
+		byte result1 = 10 + 20;
+		System.out.println(result1);
+		
+		byte x = 10;
+		byte y = 20;
+//		byte result2 = x + y; // Compile Error!
+		int result3 = x + y;
+		System.out.println(result3);
+
+	}
+
+}
