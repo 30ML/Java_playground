fd7f2f53058dfdd16952555993517db2d854daf9
Min Jae Kim <exploit017@gmail.com>
2021-12-23 21:11:32 +0900

    method done

---
+++ hongong/chap06/src/sec04/exam04_클래스_내부에서_메소드_호출/CalculatorExample.java
@@ -0,0 +1,10 @@
+package sec04.exam04_클래스_내부에서_메소드_호출;
+
+public class CalculatorExample {
+
+	public static void main(String[] args) {
+		Calculator myCalc = new Calculator();
+		myCalc.execute();
+	}
+
+}
---
+++ hongong/chap06/src/sec04/exam05_클래스_외부에서_메소드_호출/CarExample.java
@@ -0,0 +1,13 @@
+package sec04.exam05_클래스_외부에서_메소드_호출;
+
+public class CarExample {
+
+	public static void main(String[] args) {
+		Car myCar = new Car();
+		myCar.keyTurnOn();
+		myCar.run();
+		int speed = myCar.getSpeed();
+		System.out.println("현재 속도: " + speed + "km/h");
+	}
+
+}
