e8bec94c6099a65db7b50fc7072853af2b8ffff7
Minjae Jay Kim <exploit017@gmail.com>
2022-03-18 21:33:15 +0900

    int type - Infinity Test

---
+++ Lecture/HYCU-java_programming/03_Infinity/Infinity.java
@@ -0,0 +1,11 @@
+public class Infinity {
+  public static void main(String[] args) {
+    int a = 40;
+    int b = 0;
+    double c = 20;
+
+    c = a / b; // Exception
+
+    System.out.println("a=" + a + " b=" + b + " c=" + c);
+  }
+}
