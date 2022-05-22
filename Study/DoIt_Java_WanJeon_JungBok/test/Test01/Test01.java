1e25a1a4f067d176ecbd846770a59e89956e7c46
Min Jae Kim <exploit017@gmail.com>
2022-05-22 14:27:31 +0900

    update

---
+++ Study/DoIt_Java_WanJeon_JungBok/test/Test01/Test01.java
@@ -0,0 +1,15 @@
+public class Test01 {
+  public static void main(String[] args) {
+    char value1 = 'a';
+    char value2 = 97;
+    char value3 = 0b1100001;
+    char value4 = 0141;
+    char value5 = 0x61;
+
+    System.out.println(value1);
+    System.out.println(value2);
+    System.out.println(value3);
+    System.out.println(value4);
+    System.out.println(value5);
+  }
+}
