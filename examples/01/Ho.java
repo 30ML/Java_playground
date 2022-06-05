fd394ff33bfb50e0fae5406b774dbaab047417bb
Min Jae Kim <exploit017@gmail.com>
2022-06-05 19:54:40 +0900

    update

---
+++ examples/01/Ho.java
@@ -0,0 +1,16 @@
+public class Ho {
+  public static void main(String[] args) {
+    int num1 = 6;
+
+    int i;
+    int sum = 0;
+    for (i = 1; i <= num1; i++) {
+      sum = sum + i;
+    }
+
+    System.out.println(sum);
+    System.out.println(1 + 2 + 3 + 4 + 5 + 6);
+
+    System.out.println((int) (Math.random() * 40) + 2000);
+  }
+}
